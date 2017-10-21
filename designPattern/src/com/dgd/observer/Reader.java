d95c7e2e8c22ad8824d89946ad688c0a863571d2
dgd <123dgd@163.com>
2017-10-21 11:10:27 +0800

    添加了观察者模式的实现

---
+++ designPattern/src/com/dgd/observer/Reader.java
@@ -0,0 +1,23 @@
+package com.dgd.observer;
+
+/**
+ * @Author DGD
+ * @date 2017/10/21.
+ * 读者对象,实现了观察者接口
+ */
+public class Reader implements Observer {
+    //读者的姓名
+    private String name;
+    @Override
+    public void update(Subject subject) {
+        System.out.println(name+"收到报纸了,阅读它,内容是:"+((Newspaper)subject).getContent());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
