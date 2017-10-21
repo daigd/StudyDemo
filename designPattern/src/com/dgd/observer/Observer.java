385e6622863fdb07d69fd5618e44f33b4cdedb57
dgd <123dgd@163.com>
2017-10-21 10:51:33 +0800

    修改了观察者模式的定义

--- designPattern/src/com/dgd/observer/Observer.java
+++ designPattern/src/com/dgd/observer/Observer.java
@@ -5,6 +5,9 @@ package com.dgd.observer;
  * @create 2017-10-20 23:24
  */
 public interface Observer {
-    //观察者改变状态的方法
+    /**
+     * 观察者改变状态的方法
+     * @param subject 传入目标对象,方便获取目标对象的状态
+     */
     void update(Subject subject);
 }
--- designPattern/src/com/dgd/observer/Subject.java
+++ designPattern/src/com/dgd/observer/Subject.java
@@ -6,7 +6,8 @@ import java.util.List;
 /**
  * @author DaiGuodong
  * @create 2017-10-20-23:24
- * 观察者模式的目标对象
+ * 观察者模式的目标对象:
+ * 它知道它的观察者,并提供添加观察者和删除观察者的接口
  */
 public class Subject {
     //目标对象要维护观察者的信息
@@ -20,12 +21,12 @@ public class Subject {
 
     //移除观察者
 
-    public void remove(Observer observer) {
+    public void detach(Observer observer) {
         observers.remove(observer);
     }
 
-    //给观察者发布通知
-    public void noticeToAllObservers() {
+    //通知所有注册的观察者对象
+    public void notifyObservers() {
         for (Observer observer : observers) {
             observer.update(this);
         }
