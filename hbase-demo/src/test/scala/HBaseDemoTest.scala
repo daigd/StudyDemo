c6e40882a27369e25713cc2e01cf07a8275cdd6d
daigd <123dgd@163.com>
2020-12-11 17:26:40 +0800

    spark demo

--- hbase-demo/src/test/scala/HBaseDemoTest.scala
+++ hbase-demo/src/test/scala/HBaseDemoTest.scala
@@ -1,7 +1,8 @@
-import org.apache.hadoop.hbase.HBaseConfiguration
-import org.apache.hadoop.hbase.client.{ConnectionFactory, Get, HTable, Put}
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.hbase.{HBaseConfiguration, HColumnDescriptor, HTableDescriptor, TableName}
+import org.apache.hadoop.hbase.client.{Admin, ConnectionFactory, Get, HTable, Put}
 import org.apache.hadoop.hbase.util.Bytes
-import org.scalatest.GivenWhenThen
+import org.scalatest.{BeforeAndAfter, GivenWhenThen}
 import org.scalatest.funsuite.AnyFunSuite
 
 /**
@@ -9,16 +10,20 @@ import org.scalatest.funsuite.AnyFunSuite
  * @createtime :  2020年12月08日 10:46
  * @description : HBase Demo验证
  */
-class HBaseDemoTest extends AnyFunSuite with GivenWhenThen{
+class HBaseDemoTest extends AnyFunSuite with GivenWhenThen with BeforeAndAfter{
 
   val TABLE_NAME :String = "test"
-
-  test("HBase test") {
-    val conf = HBaseConfiguration.create
+  val conf: Configuration = HBaseConfiguration.create
+  
+  //before(init())
+  def getAdmin(): Admin ={
     conf.set("hbase.zookeeper.quorum", "bigdata-01.com")
     val conn = ConnectionFactory.createConnection(conf)
-    val admin = conn.getAdmin
-
+    conn.getAdmin
+  }
+  
+  test("HBase test") {
+    val admin = getAdmin()
     // list the tables
     val listTables=admin.listTables()
     listTables.foreach(println)
@@ -39,5 +44,43 @@ class HBaseDemoTest extends AnyFunSuite with GivenWhenThen{
     val value=result.value()
     println(Bytes.toString(value))
     assert(Bytes.toString(value) == "Jack")
+  }
+  
+  test("HBase 创建表") {
+    Given("实例化Admin")
+    val admin = getAdmin()
+    
+    Given("创建TableDescriptor")
+    
+    val table = new HTableDescriptor(TableName.valueOf("demo"))
+    val family = new HColumnDescriptor(Bytes.toBytes("info"))
+    table.addFamily(family)
+    
+    Given("创建表")
+    admin.createTable(table)
+    
+  }
+  
+  test("禁用表"){
+    Given("判断是否被禁用")
+    val admin = getAdmin()
+    val tableName = TableName.valueOf("demo")
+    var isDisabled = admin.isTableDisabled(tableName)
+    if(!isDisabled) {
+      admin.disableTable(tableName)
+      isDisabled = admin.isTableDisabled(tableName)
+    }
+    assert(isDisabled)
+  }
+  
+  test("启用表"){
+    val admin = getAdmin()
+    val tableName = TableName.valueOf("demo")
+    var isDisabled = admin.isTableDisabled(tableName)
+    if(isDisabled){
+      admin.enableTable(tableName)
+      isDisabled = admin.isTableDisabled(tableName)
+    }
+    assert(!isDisabled)
   }
 }
---
+++ spark-demo/src/main/scala/HelloWorld.scala
@@ -0,0 +1,10 @@
+/**
+ * @author : DaiGD
+ * @createtime :  2020年12月10日 17:20
+ * @description : 
+ */
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello World!")
+  }
+}
